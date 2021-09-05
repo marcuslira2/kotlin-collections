b84d0d59993e51b79aad94f8f773fc3aff264f2e
Marcus Lira <marcuslira57@gmail.com>
2021-09-05 17:46:29 -0300

    MultableList class created

---
+++ src/main/kotlin/one/digitalinnovation/collections/TestMutableList.kt
@@ -0,0 +1,25 @@
+package one.digitalinnovation.collections
+
+fun main(){
+    val joao  = Empregado ("João", 1000.0,"clt")
+    val maria = Empregado ("Maria", 2000.0,"pj")
+    val pedro = Empregado ("Pedro", 3000.0,"pj")
+
+    val funcionarios = mutableListOf(joao, maria)
+    funcionarios.forEach { println(it) }
+    println("----------")
+
+    //SET
+
+    funcionarios.add(pedro)
+    funcionarios.forEach { println(it) }
+
+    println("----------")
+
+    //DELET
+    
+    funcionarios.remove(joao)
+    funcionarios.forEach { println(it) }
+
+
+}
